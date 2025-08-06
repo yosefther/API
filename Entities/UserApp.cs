b3565645647543105b3f389752360e62afea57c0
yosefther <yousefthair9@gmail.com>
2025-08-06 23:51:26 +0300

    init user entity file + add usr req

--- Entities/UserApp.cs
+++ Entities/UserApp.cs
@@ -1,8 +1,17 @@
+// Entity Framework (EF) ==> it dose  mapp your application's data models to your database
+
 using System;
 
 namespace API.Entities;
 
 public class UserApp
 {
+    public string? Id { get; set; } = Guid.NewGuid().ToString();
+    // required keyword ==> t's used to enforce initialization of properties and fields within a class, record, or struct.  
+    public required string? DisplayName { get; set; } = "";
+
+    public required string? Email { get; set; } = "";
 
 }
+
+// later i will add more code to it after i read more about "EF"
